0e9943697d623bfb7b4372383596df8be918f066
user <localhost@localhost>
2025-06-04 18:41:14 +0300

    The webpack configuration (client part) has been improved - support for disabling minification and sourcemap generation in the development environment has been added

--- src_client/webpack.config.js
+++ src_client/webpack.config.js
@@ -3,31 +3,37 @@ const path = require("path");
 const LIBRARY_NAME = 'redage-clientside';
 const OUTPUT_FILE = 'main.js';
 
-module.exports = mode => ({
-    entry: './index.js',
-    mode,
-    /*module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
+module.exports = (env, argv) => {
+    const mode = argv.mode || 'development';
+    const isProduction = mode === 'production';
 
-            }
-        ]
-    },*/
-    optimization: {
-        minimize: true
-    },
-    performance: {
-        hints: false
-    },
-    output: {
-		//path: path.join(__dirname, "public"),
-        path: path.join(__dirname, "../client_packages"),
-        filename: OUTPUT_FILE,
-        library: LIBRARY_NAME,
-        libraryTarget: 'umd',
-        globalObject: 'global',
+    return {
+        entry: './index.js',
+        mode,
+        /*module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader',
+
+                }
+            ]
+        },*/
+        optimization: {
+            minimize: isProduction
+        },
+        performance: {
+            hints: false
+        },
+        output: {
+            //path: path.join(__dirname, "public"),
+            path: path.join(__dirname, "../client_packages"),
+            filename: OUTPUT_FILE,
+            library: LIBRARY_NAME,
+            libraryTarget: 'umd',
+            globalObject: 'global',
+        },
+        devtool: isProduction ? false : 'source-map'
     }
-});
+};
